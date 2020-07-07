cca0f2fb32a3e967f1ccdabd95522671a01617aa
omarouafi <omarouafi12@gmail.com>
2020-07-07 17:29:58 +0000

    Updates

---
+++ precache-manifest.db6790a13c803166a5b5f6d000b374ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192c7b8c893dfadcd0cbc068128010ff",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "d22e35e03aed59b9c254",
+    "url": "/monster/static/css/main.e49eca8a.chunk.css"
+  },
+  {
+    "revision": "2b4c1199f1e002dcdb8b",
+    "url": "/monster/static/js/2.e1eeb85d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.e1eeb85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22e35e03aed59b9c254",
+    "url": "/monster/static/js/main.6d5327f1.chunk.js"
+  },
+  {
+    "revision": "f3e8bcfb4e52c791a4b3",
+    "url": "/monster/static/js/runtime-main.ff825015.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster/static/media/logo.5d5d9eef.svg"
+  }
+]);
