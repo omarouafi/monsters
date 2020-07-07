71c9b9f469f28f1292a6402ba84769d302de4de2
omarouafi <omarouafi12@gmail.com>
2020-07-07 16:43:30 +0000

    update

---
+++ react-app/src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './card.component.styles.css'
+
+export const Card = (props) =>{
+    return <div className="card-container">
+        <img src={`https://robohash.org/${props.monster.id}?size=180x180`} alt=""/>
+        <h2>{props.monster.name}</h2>
+        <p>{props.monster.email}</p>
+    </div>
+}
